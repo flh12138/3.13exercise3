73caa459ed4860b92869c7e9bf73796e637c07f6
FLH0202 <FLH0202@163.com>
2020-03-13 14:50:56 +0800

    lal

---
+++ 3.13exercise3/main.cpp
@@ -0,0 +1,41 @@
+#include<opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+string window_name = "binaryMat";
+
+void threshod_Mat(int th, void* data)
+{
+    Mat src = *(Mat*)(data);
+    Mat dst;
+
+   //二值化
+    threshold(src, dst, th, 255, 0);
+   imshow(window_name, dst);
+}
+
+int main()
+{
+	Mat srcMat;
+	Mat gryMat;
+	int lowTh = 30;
+	int maxTh = 255;
+	srcMat = imread("E:\\PIC\\112.jpg");
+	if (!srcMat.data) //判断图像是否载入
+	{
+		cout << "图像载入失败!" << endl;
+		return 0;
+	}
+	// imshow (window_ name, Image) ;
+	cvtColor(srcMat, gryMat, CV_BGR2GRAY);
+	imshow(window_name, gryMat);
+	createTrackbar(" threshold",
+		   window_name,
+		   &lowTh,
+		   maxTh,
+		   threshod_Mat,
+		   &gryMat);
+	waitKey(0);
+	return 0;
+}
